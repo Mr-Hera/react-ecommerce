b5875107f72dc1e87f6eeb1d37445294116b63b4
Mr-Hera <randellhera@gmail.com>
2023-02-15 20:42:03 +0300

    Feat: added two-way bidning to login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ class Login extends Component{
             password: "password",
         };
     }
+
     render() {
         return(
             <div className='col-lg-9 p-2'>
@@ -18,16 +19,24 @@ class Login extends Component{
                 {/* Email */}
                 <div className="form-group form-row p-2">
                     <label htmlFor="email" className="col-lg-4">Email:</label>
-                    <input name="email" type="text" className="form-control" value={this.state.email} />
+                    <input name="email" type="text" className="form-control" value={this.state.email} onChange={(e) => {this.setState({ email: e.target.value })}} />
                 </div>
 
                 {/* Password */}
                 <div className="form-group form-row p-2">
                     <label htmlFor="password" className="col-lg-4">Password:</label>
-                    <input name="password" type="password" className="form-control" value={this.state.password} />
+                    <input name="password" type="password" className="form-control" value={this.state.password} onChange={(e) => {this.setState({ password: e.target.value })}} />
                 </div>
+
+                <button className="btn btn-primary m-2" onClick={this.onLoginClick}>
+                    Login
+                </button>
             </div>
         );
+    }
+
+    onLoginClick = () => {
+        console.log(this.state);
     }
 }
 
