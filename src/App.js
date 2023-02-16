f652443f13cedc17a7a57de513b97ea79eee48df
Mr-Hera <randellhera@gmail.com>
2023-02-16 22:09:22 +0300

    Feat: added routing feature

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
-// import CustomersList from './components/CustomersList'
-// import ShoppingCart from './components/ShoppingCart'
+import CustomersList from './components/CustomersList'
+import ShoppingCart from './components/ShoppingCart'
 import Login from './components/Login'
+import Dashboard from './components/Dashboard'
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
-      <Login />
-      {/* <ShoppingCart /> */}
-      {/* <CustomersList /> */}
-    </>
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/dashboard" exact element={<Dashboard />} />
+        <Route path="/shopping-cart" exact element={<ShoppingCart />} />
+        <Route path="/customer-list" exact element={<CustomersList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
