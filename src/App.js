30af233dca4c9124a288245eddc9ea89ece3a3c6
Mr-Hera <randellhera@gmail.com>
2023-02-16 23:11:16 +0300

    Feat: added a 404 page + routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CustomersList from './components/CustomersList'
 import ShoppingCart from './components/ShoppingCart'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
+import NoMatchFound from './components/NoMatchFound'
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/dashboard" exact element={<Dashboard />} />
         <Route path="/shopping-cart" exact element={<ShoppingCart />} />
         <Route path="/customer-list" exact element={<CustomersList />} />
+        <Route path="*" element={<NoMatchFound />} />
       </Routes>
     </BrowserRouter>
   );
